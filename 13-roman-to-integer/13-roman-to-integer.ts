72f550599cec1f64c35c9d8461438beec8664576
codewithrema <106871822+codewithrema@users.noreply.github.com>
2022-08-04 03:43:26 +0800

    Time: 212 ms (57.36%), Space: 47.6 MB (82.76%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.ts
+++ 13-roman-to-integer/13-roman-to-integer.ts
@@ -12,14 +12,13 @@ const SYMBOL_LIST = Object.keys(SYMBOL_MAP);
 
 function romanToInt(s: string): number {
   return [...s].reduce((acc, cur, i, arr) => {
-    if (i === 0) {
-      return SYMBOL_MAP[cur];
+    if (
+      !arr[i + 1] ||
+      SYMBOL_LIST.indexOf(cur) >= SYMBOL_LIST.indexOf(arr[i + 1])
+    ) {
+      return acc + SYMBOL_MAP[cur];
     }
 
-    if (SYMBOL_LIST.indexOf(arr[i - 1]) >= SYMBOL_LIST.indexOf(cur)) {
-      return acc + SYMBOL_MAP[cur];
-    } else {
-      return acc - SYMBOL_MAP[arr[i - 1]] * 2 + SYMBOL_MAP[cur];
-    }
+    return acc - SYMBOL_MAP[cur];
   }, 0);
 }
